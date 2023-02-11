0b8ae6fa3a2c3de35e22ffbfa28cd69f7514c051
thepjos <josh.thephasdin@gmail.com>
2023-02-11 00:08:48 -0600

    js done

--- script.js
+++ script.js
@@ -5,16 +5,32 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+// USING ASYNC AWAIT
+async function generateJoke() {
     const config = {
         headers: {
             'Accept': 'application/json',
         },
     }
-    fetch('https://icanhazdadjoke.com', config)
-        .then(res => res.json()
-        .then(data => {
-            jokeEl.innerHTML = data.joke
-        }
-    ))
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+
 }
+
+// USING .THEN
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json',
+//         },
+//     }
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json()
+//         .then(data => {
+//             jokeEl.innerHTML = data.joke
+//         }
+//     ))
+// }
